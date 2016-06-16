21ddd35f75751ea080757ddcd315583723671046
Juan Arias <juan.arias@bluetab.net>
2016-06-16 18:03:22 +0200

    Minor refactor, to make bash linter happy

--- start.sh
+++ start.sh
@@ -14,14 +14,14 @@ ENDCOLOR=$(tput sgr0)
 BACKUP_FOLDER="${HOME}/backup"
 
 # return the absolute path of a local file
-dotfiles.absolute() {
+dotfiles_absolute() {
   local file=$1
   echo "$(cd $(dirname $file) && pwd)/$(basename $file)"
 }
 
 # Easily choose the link method between a symbolic link or a copy
-dotfiles.link() {
-  local local_dotfile=$(dotfiles.absolute "$1")
+dotfiles_link() {
+  local local_dotfile=$(dotfiles_absolute "$1")
   local system_dotfile="$2"
 
   [ ! -d $BACKUP_FOLDER ] && mkdir -p $BACKUP_FOLDER
@@ -32,16 +32,16 @@ dotfiles.link() {
   ln -sf $local_dotfile $system_dotfile
 }
 
-dotfiles.error() {
+dotfiles_error() {
   echo "[ERROR] $@"
   exit 1
 }
 
-setup.git() {
-  echo -e "${GREENCOLOR}Setting up some git defaults.....${ENDCOLOR}"
+setup_git() {
+  echo "${GREENCOLOR}Setting up some git defaults.....${ENDCOLOR}"
   while read line ; do
 
-    echo -e "${BLUECOLOR}${line}${ENDCOLOR}"
+    echo "${BLUECOLOR}${line}${ENDCOLOR}"
     section=$(echo $line | cut -d" " -f1)
     value=$(echo $line | cut -d" " -f2-)
 
@@ -49,45 +49,45 @@ setup.git() {
   done < .gitconfig
 }
 
-setup.vim() {
+setup_vim() {
   VIMDIR=~/.vim
-  echo -e "${GREENCOLOR}Setting up VIM in ${VIMDIR} ...${ENDCOLOR}"
+  echo "${GREENCOLOR}Setting up VIM in ${VIMDIR} ...${ENDCOLOR}"
   rm -rf ${VIMDIR}/bundle
   mkdir -p ${VIMDIR}/tmp ${VIMDIR}/backup ${VIMDIR}/colors > /dev/null 2>&1
   cp .vim/colors/monokai.vim ${VIMDIR}/colors/
   git clone https://github.com/gmarik/Vundle.vim ${VIMDIR}/bundle/Vundle.vim
-  dotfiles.link .vimrc ~/.vimrc
+  dotfiles_link .vimrc ~/.vimrc
   vim +PluginInstall +qall
 }
 
-setup.dotfiles() {
-  echo -e "${GREENCOLOR}Setting up bash dotfiles.....${ENDCOLOR}"
-  [[ $(grep -c "\. ~/.bashrc" ~/.bash_profile) -ne 1 ]] && cat >> ~/.bash_profile << _EOF
+setup_dotfiles() {
+  echo "${GREENCOLOR}Setting up bash dotfiles_....${ENDCOLOR}"
+  [ $(grep -c "\. ~/.bashrc" ~/.bash_profile) -ne 1 ] && cat >> ~/.bash_profile << _EOF
 # Use my all-powerful bashrc file (remove this to deactivate)
 if [ -f ~/.bashrc ]; then
   . ~/.bashrc
 fi
 _EOF
 
-  dotfiles.link .bashrc ~/.bashrc
-  dotfiles.link .bash_local_aliases ~/.bash_local_aliases
+  dotfiles_link .bashrc ~/.bashrc
+  dotfiles_link .bash_local_aliases ~/.bash_local_aliases
   . ~/.bash_profile
 }
 
-setup.postgres() {
-  echo -e "${GREENCOLOR}Setting up some PostgreSQL defaults.....${ENDCOLOR}"
-  dotfiles.link .psqlrc ~/.psqlrc
+setup_postgres() {
+  echo "${GREENCOLOR}Setting up some PostgreSQL defaults.....${ENDCOLOR}"
+  dotfiles_link .psqlrc ~/.psqlrc
 }
 
-setup.configs() {
-  echo -e "${GREENCOLOR}Setting up some environment-specific configs.....${ENDCOLOR}"
+setup_configs() {
+  echo "${GREENCOLOR}Setting up some environment-specific configs.....${ENDCOLOR}"
   case "$ENV_TYPE" in
     "prod")
       ;;
     "dev-server")
       ;;
     "local")
-    dotfiles.link .config/terminator/config $HOME/.config/terminator/config
+    dotfiles_link .config/terminator/config $HOME/.config/terminator/config
       ;;
   esac
 }
@@ -95,28 +95,28 @@ setup.configs() {
 # local       -> one of my computers, where I'd like to use Terminator, pretty nice colors, and the likes [DEFAULT]
 # dev-server  -> a remote development computer, no local configs are needed (such as terminator)
 # prod        -> a production environment, where I most probably don't need fancy colors, just some basics
-setup.env() {
+setup_env() {
   ENV_TYPE="$1"
   if [ -z "$ENV_TYPE" ] ; then
     ENV_TYPE="local" # not using a bash default above, so the message is more explicit
-    echo -e "${GREENCOLOR}No environment specified, defaulting to: ${ENDCOLOR}${BLUECOLOR_BOLD}${ENV_TYPE}${ENDCOLOR}"
+    echo "${GREENCOLOR}No environment specified, defaulting to: ${ENDCOLOR}${BLUECOLOR_BOLD}${ENV_TYPE}${ENDCOLOR}"
   else
     case "$ENV_TYPE" in
       prod|dev-server|local)
         ;;
       *)
-        dotfiles.error "Invalid environment selected '${REDCOLOR_BOLD}$ENV_TYPE${ENDCOLOR}'"
+        dotfiles_error "Invalid environment selected '${REDCOLOR_BOLD}$ENV_TYPE${ENDCOLOR}'"
     esac
-    echo -e "${GREENCOLOR}Environment set to: ${ENDCOLOR}${BLUECOLOR_BOLD}${ENV_TYPE}${ENDCOLOR}"
+    echo "${GREENCOLOR}Environment set to: ${ENDCOLOR}${BLUECOLOR_BOLD}${ENV_TYPE}${ENDCOLOR}"
   fi
   export ENV_TYPE
 }
 
-setup.env $@
-setup.vim
-setup.dotfiles
-setup.git
-setup.postgres
-setup.configs
+setup_env $@
+setup_vim
+setup_dotfiles
+setup_git
+setup_postgres
+setup_configs
 
-echo -e "${GREENCOLOR_BOLD}Everything is done, enjoy!${ENDCOLOR}"
+echo "${GREENCOLOR_BOLD}Everything is done, enjoy!${ENDCOLOR}"
